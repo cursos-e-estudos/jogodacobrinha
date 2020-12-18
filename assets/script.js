78e9b5a98e8718d0a0d7ff620f4008d9a3cbe3d1
Biel <vgabriel@id.uff.br>
2020-12-18 16:49:55 -0300

    função de criar cobrinha feita e ela se move;

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,21 @@
+// Variáveis do canvas do HTML
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
+
+// tamanho das caixas?
 let box = 32;
+
+// Configurando a cobrinha
+let snake = [];
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
+};
+
+// direcao da cobrinha
+let direcao = "right";
+
+
 
 function criarBG()
 {
@@ -9,5 +24,51 @@ function criarBG()
     
 }
 
+function criarCobrinha()
+{
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+        
+    }
+}
 
-criarBG();
+function iniciarJogo()
+{
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    criarBG();
+    criarCobrinha();
+
+    if(direcao == "right")
+    {
+        snakeX += box;
+    }
+
+    if(direcao == "left")
+    {
+        snakeX -= box;
+    }
+
+    if(direcao == "up")
+    {
+        snakeY -= box;
+    }
+
+    if(direcao == "down")
+    {
+        snakeY += box;
+    }
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    };
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
